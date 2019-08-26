170068b60ed0983328a1b98a03d6ec8ee7e883ac
Kevin Seidel <kevin.seidel@karrieretutor.de>
2019-08-26 16:13:51 +0200

    added doc and removed unnecessary method

--- exporter/stats.go
+++ exporter/stats.go
@@ -2,16 +2,16 @@ package main
 
 import "encoding/xml"
 
+// Stats Container for individual stats as they are broadcasted by the JVBs.
+// Stats is then registered as gosrc.io/xmpp/stanza.PresExetension which unmarshals
+// the xml coming in via xmpp into these Stat structs
 type Stats struct {
 	XMLName xml.Name `xml:"http://jitsi.org/protocol/colibri stats"`
 	Stats   []Stat   `xml:"stat"`
 }
 
+// Stat Individual stat containing a single metric, such as Name="bit_rate_upload", Value="0"
 type Stat struct {
 	Name  string `xml:"name,attr"`
 	Value string `xml:"value,attr"`
-}
-
-func (s Stats) Namespace() string {
-	return s.XMLName.Space
 }
